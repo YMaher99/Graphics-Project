38c81f976cc6cad9c7b3f807329c6bf527637866
MostafaXLotfy <mostafaXlotfy@gmail.com>
2023-01-16 22:04:48 +0200

    add grass that is not working

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.136";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.136/examples/jsm/loaders/GLTFLoader.js";
-import { GUI } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/libs/lil-gui.module.min.js';
-import { Water } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/objects/Water.js';
+import { GUI } from "https://cdn.skypack.dev/three@0.136/examples/jsm/libs/lil-gui.module.min.js";
+import { EffectComposer } from "https://cdn.skypack.dev/three@0.136/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "https://cdn.skypack.dev/three@0.136/examples/jsm/postprocessing/RenderPass.js";
+import { FilmPass } from "https://cdn.skypack.dev/three@0.136/examples/jsm/postprocessing/FilmPass.js";
+import { BloomPass } from "https://cdn.skypack.dev/three@0.136/examples/jsm/postprocessing/BloomPass.js";
 
-alert("Controls: Use A to go left and D to go right\nHow To Play: avoid the monster or collect the mushroom to get a shield that allows you to kill the monster.\nGet score by surviving and killing the mosnter")
+// alert("Controls: Use A to go left and D to go right\nHow To Play: avoid the monster or collect the mushroom to get a shield that allows you to kill the monster.\nGet score by surviving and killing the mosnter")
 
 // Set up the scene
 let scene;
 let camera;
 let renderer;
-let geometry;
 let material;
 let plane;
 let coinGeometry;
@@ -27,8 +29,9 @@ let playSFX = false;
 let enemyMixer;
 let playerMixer;
 
-const ambient = new THREE.AmbientLight(0xffffff);
+const ambient = new THREE.AmbientLight(0xffffff, 0.5);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+
 const gltfLoader = new GLTFLoader();
 const listener = new THREE.AudioListener();
 const audioLoader = new THREE.AudioLoader();
@@ -62,7 +65,7 @@ audioLoader.load("../assets/sounds/powerDownSound.mp3", function (buffer) {
   poweringDownSound.setLoop(false);
   poweringDownSound.setVolume(0.1);
 });
-audioLoader.load("../assets/sounds/defeat.mp3", function(buffer){
+audioLoader.load("../assets/sounds/defeat.mp3", function (buffer) {
   defeatSound.setBuffer(buffer);
   defeatSound.setLoop(false);
   defeatSound.setVolume(0.5);
@@ -71,7 +74,7 @@ audioLoader.load("../assets/sounds/defeat.mp3", function(buffer){
 // Add the coin to the scene
 
 function createPlane() {
-  geometry = new THREE.PlaneGeometry(50, 1000, 100, 100);
+  const geometry = new THREE.PlaneGeometry(35, 1000, 100, 100);
   const texture = new THREE.TextureLoader().load("../assets/asphalt.jpg");
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
@@ -100,7 +103,6 @@ function createScene() {
 
   document.body.appendChild(renderer.domElement);
   //scene.add(coinMesh);
-
 
   camera.rotation.x = Math.PI / 2;
 
@@ -149,7 +151,7 @@ function loadSpaceShip() {
 
     // Get the animations from the GLTF file
     let animations = gltf.animations;
-    playerMixer.clipAction(animations[0]).play()
+    playerMixer.clipAction(animations[0]).play();
     // create the sphere geometry
     const sphereGeometry = new THREE.SphereGeometry(
       4,
@@ -182,14 +184,11 @@ function loadSpaceShip() {
 }
 
 // Animate the scene
-let clock = new THREE.Clock()
+let clock = new THREE.Clock();
 function animate() {
-
   requestAnimationFrame(animate);
 
-
-
-  let delta = clock.getDelta()
+  let delta = clock.getDelta();
   if (poweredUp) {
     sphere.position.set(playerModel.position.x, 2, -0.5);
   }
@@ -197,11 +196,15 @@ function animate() {
   enemyModel.position.y -= 0.1;
   mushroomModel.position.y -= 0.1;
 
-  if(destination - playerModel.position.x > 0.5){playerModel.rotation.y =  Math.PI - Math.PI/6}
-  else if (destination - playerModel.position.x < -0.5){playerModel.rotation.y =  Math.PI + Math.PI/6}
-  else{playerModel.rotation.y =  Math.PI}
+  if (destination - playerModel.position.x > 0.5) {
+    playerModel.rotation.y = Math.PI - Math.PI / 6;
+  } else if (destination - playerModel.position.x < -0.5) {
+    playerModel.rotation.y = Math.PI + Math.PI / 6;
+  } else {
+    playerModel.rotation.y = Math.PI;
+  }
 
-  playerModel.position.x += (destination - playerModel.position.x) / 20
+  playerModel.position.x += (destination - playerModel.position.x) / 20;
   camera.position.x = playerModel.position.x;
 
   if (plane.position.y < -50) {
@@ -219,21 +222,18 @@ function animate() {
       poweredUp = false;
       sphere.position.set(0, 2, -10);
       score += 100;
-      score_folder.controllers[0].setValue(Math.round(score))
+      score_folder.controllers[0].setValue(Math.round(score));
       if (playSFX) {
         poweringDownSound.play();
       }
+    } else {
+      defeatSound.play();
+      alert("YOU LOST");
+      location.reload();
     }
-    else{
-      defeatSound.play()
-      alert("YOU LOST")
-      location.reload()
-    }
-
   }
-  score += 0.5
-  score_folder.controllers[0].setValue(Math.round(score))
-
+  score += 0.5;
+  score_folder.controllers[0].setValue(Math.round(score));
 
   if (isColliding(playerModel, mushroomModel)) {
     poweredUp = true;
@@ -243,10 +243,12 @@ function animate() {
       poweringUpSound.play();
     }
   }
-  enemyMixer.update(delta)
-  playerMixer.update(delta)
+	leavesMaterial.uniforms.time.value = clock.getElapsedTime();
+  leavesMaterial.uniformsNeedUpdate = true;
+  enemyMixer.update(delta);
+  playerMixer.update(delta);
+  renderer.clear()
   renderer.render(scene, camera);
-
 }
 
 function createSkyBox() {
@@ -297,7 +299,6 @@ function initUI() {
       runningSound.play();
       playSFX = true;
     }
-    
   };
   playSFX = false;
   onSoundToggle();
@@ -332,8 +333,8 @@ window.addEventListener("keydown", (event) => {
 
 function random_coord() {
   return Math.random() > 0.5
-    ? Math.floor((Math.random() * 3) + 1) * 5
-    : -1 * Math.floor((Math.random() * 3)+1) * 5;
+    ? Math.floor(Math.random() * 3 + 1) * 5
+    : -1 * Math.floor(Math.random() * 3 + 1) * 5;
 }
 
 gltfLoader.load("../assets/player/enemy.glb", function (gltf) {
@@ -346,10 +347,7 @@ gltfLoader.load("../assets/player/enemy.glb", function (gltf) {
 
   // Get the animations from the GLTF file
   let animations = gltf.animations;
-  enemyMixer.clipAction(animations[2]).play()
-
-
-
+  enemyMixer.clipAction(animations[2]).play();
 
   enemyModel.position.set(random_coord(), 40, 0);
   enemyModel.rotation.x = Math.PI / 2;
@@ -370,18 +368,109 @@ gltfLoader.load("../assets/mushroom.glb", function (gltf) {
 
 let gui = new GUI();
 var myObject = {
-	Score: 0,
+  Score: 0,
 };
 var score_folder = gui.addFolder("Score");
-score_folder.add(myObject,"Score")
+score_folder.add(myObject, "Score");
 var controls = {
-	Left: "A",
-	Right:"D"
+  Left: "A",
+  Right: "D",
+};
+gui.title("");
+var controls_folder = gui.addFolder("Controls");
+controls_folder.add(controls, "Left");
+controls_folder.add(controls, "Right");
+
+const vertexShader = `
+  varying vec2 vUv;
+  uniform float time;
+  
+	void main() {
+
+    vUv = uv;
+    
+    // VERTEX POSITION
+    
+    vec4 mvPosition = vec4( position, 1.0 );
+    #ifdef USE_INSTANCING
+    	mvPosition = instanceMatrix * mvPosition;
+    #endif
+    
+    // DISPLACEMENT
+    
+    // here the displacement is made stronger on the blades tips.
+    float dispPower = 1.0 - cos( uv.y * 3.1416 / 2.0 );
+    
+    float displacement = sin( mvPosition.z + time * 10.0 ) * ( 0.1 * dispPower );
+    mvPosition.z += displacement;
+    
+    //
+    
+    vec4 modelViewPosition = modelViewMatrix * mvPosition;
+    gl_Position = projectionMatrix * modelViewPosition;
+
+	}
+`;
+
+const fragmentShader = `
+  varying vec2 vUv;
+  
+  void main() {
+  	vec3 baseColor = vec3( 0.41, 1.0, 0.5 );
+    float clarity = ( vUv.y * 0.5 ) + 0.5;
+    gl_FragColor = vec4( baseColor * clarity, 1 );
+  }
+`;
+
+const uniforms = {
+	time: {
+  	value: 0
+  }
 }
-gui.title("")
-var controls_folder = gui.addFolder("Controls")
-controls_folder.add(controls,"Left")
-controls_folder.add(controls,"Right")
+
+const leavesMaterial = new THREE.ShaderMaterial({
+	vertexShader,
+  fragmentShader,
+  uniforms,
+  side: THREE.DoubleSide
+});
+
+/////////
+// MESH
+/////////
+
+const instanceNumber = 2 * 5000;
+const dummy = new THREE.Object3D();
+
+const geometry = new THREE.PlaneGeometry( 0.1, 3, 1, 4 );
+geometry.translate( 0, 0.5, 0 ); // move grass blade geometry lowest point at 0.
+
+const instancedMesh = new THREE.InstancedMesh( geometry, leavesMaterial, instanceNumber );
+
+
+// Position and scale the grass blade instances randomly.
+
+for ( let i=0 ; i<instanceNumber ; i++ ) {
+
+	dummy.position.set(
+  	( Math.random() - 0.5 ) * 10,
+    0,
+    ( Math.random() - 0.5 ) * 10
+  );
+  
+  dummy.scale.setScalar( 0.5 + Math.random() * 0.5 );
+  
+  dummy.rotation.y = Math.random() * Math.PI;
+  
+  dummy.updateMatrix();
+  instancedMesh.setMatrixAt( i, dummy.matrix );
+
+}
+
+instancedMesh.position.set(25, 0, 0)
+instancedMesh.rotation.x = Math.PI / 2
+instancedMesh.scale.set(1, 1, 1)
+scene.add( instancedMesh );
 
 
 initUI();
